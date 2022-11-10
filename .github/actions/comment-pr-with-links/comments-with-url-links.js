a171988ac17f6e53a0a1c458f32a2947bb129788
benjaminParisel <benjamin.parisel@bonitasoft.com>
2022-11-10 16:35:03 +0100

    wording

--- .github/actions/comment-pr-with-links/comments-with-url-links.js
+++ .github/actions/comment-pr-with-links/comments-with-url-links.js
@@ -22,7 +22,7 @@ module.exports = {
     },
     createOrUpdateComments: async function ({github,context}){
         let {LINKS, RENAMED_FILES, HAS_DELETED_FILES} = process.env;
-        const header='## :memo: Pull request files update\n\n';
+        const header='## :memo: Check the pages that have been modified\n\n';
         let body = buildMessage(header,LINKS,HAS_DELETED_FILES === 'true' || RENAMED_FILES != '');
         const {exists, id} = await githubUtils.isCommentExist({github,context,header});
         // Delete oldest comment if another comments exist
@@ -37,11 +37,11 @@ function buildMessage(header,links,hasWarningMessage){
     const preface =
         'In order to merge this pull request, you need to check your updates with the following url.\n\n';
 
-    const availableLinks = `### :mag: Url to check: \n ${links}\n\n\n\n`;
+    const availableLinks = `### :mag: Page list: \n ${links}\n\n\n\n`;
     //Adding deleted or renamed check
     let warningAliasMessage = '';
     if(hasWarningMessage){
-        warningAliasMessage='\n \n ### :warning: At least one file are deleted on this pull request, be sure to adding [alias](https://github.com/bonitasoft/bonita-documentation-site/blob/master/docs/content/CONTRIBUTING.adoc#use-alias-to-create-redirects)'    }
+        warningAliasMessage='\n \n ### :warning: At least one page has been deleted in the Pull Request. Make sure to add [aliases](https://github.com/bonitasoft/bonita-documentation-site/blob/master/docs/content/CONTRIBUTING.adoc#use-alias-to-create-redirects)'    }
 
     return header + preface + availableLinks + warningAliasMessage;
 }
