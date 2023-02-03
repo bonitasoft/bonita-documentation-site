4b271b9c0c91e25975caee487bbd85e5aa4e0371
Thomas Bouffard <27200110+tbouffard@users.noreply.github.com>
2023-02-03 16:33:06 +0100

    extra: preview build script - order the paths when building for test resources,

--- scripts/generate-content-for-preview-antora-playbook.js
+++ scripts/generate-content-for-preview-antora-playbook.js
@@ -105,7 +105,6 @@ else if (useTestSources) {
         url: './',
         branches: ['HEAD'],
         start_paths: [
-            'test/documentation-content/central/1.0',
             'test/documentation-content/bcd/3.4',
             'test/documentation-content/bcd/3.5',
             'test/documentation-content/bonita/v0',
@@ -115,6 +114,7 @@ else if (useTestSources) {
             'test/documentation-content/bonita/v2021.2',
             'test/documentation-content/bonita/v2022.1-beta',
             'test/documentation-content/bonita/v2022.2-alpha',
+            'test/documentation-content/central/1.0',
             'test/documentation-content/cloud/latest',
             'test/documentation-content/labs/latest',
             'test/documentation-content/test-toolkit/1.0',
